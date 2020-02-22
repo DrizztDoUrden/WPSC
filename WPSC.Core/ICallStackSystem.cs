2fc1ceb32d06ef9af28fd9f2be1e8bdc42e91c63
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-02-22 17:09:58 +0300

    Implemented call stack system interface

---
+++ WPSC.Core/ICallStackSystem.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace WPSC.Core
+{
+    public interface ICallStackSystem
+    {
+        public void IncludeLibrary(TextWriter target);
+        public void ProcessFile(string fileName, TextReader source, TextWriter target);
+    }
+}
